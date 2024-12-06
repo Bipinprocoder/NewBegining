f69d7026179754a43d36ffd864d16553cd49d547
Bipinprocoder <itsmebipin01@gmail.com>
2024-12-06 19:14:23 +0530

    Add files via upload
    This is a combination of java script and html based visual designing in this design you'll find some random random Bubbles coming out from bottom of your screen and dispaering after some time.

---
+++ BUBBLE/script.js
@@ -0,0 +1,18 @@
+const createBubble = () =>{
+    const bubble =document.createElement('div');
+    bubble.classList.add('bubble');
+    
+    const size = Math.random () + 20 + 30 + 'px';
+    bubble.style.width=size;
+    bubble.style.height=size;
+    bubble.style.left=Math.random() * 100 + 'vw';
+
+    bubble.style.animationDuration = Math.random() * 3 + 4 + 's';
+    document.body.appendChild(bubble);
+    setTimeout(() =>{
+        bubble.remove();
+
+    }, 6000);
+
+};
+setInterval(createBubble, 150);
